59548f802b10de3df2193e61872d2ae92b92cd98
Sergio Hurtarte <hurtartesergio@gmail.com>
2018-02-26 11:21:41 -0600

    este es el comit 2 de ciclos

---
+++ OperacionesBasicas/src/ciclos/Solution4.java
@@ -0,0 +1,48 @@
+package ciclos;
+
+import java.util.Scanner;
+
+public class Solution4 {
+
+	public static void main(String[] args) {
+		
+		int num1=0;
+		int num2=0;
+		int num3=0;
+		int num4=0;
+		int num5=0;
+		 
+		int promedio=0;
+		
+		
+		Scanner entrada=new Scanner(System.in);
+		System.out.println("Ingrese numero 1");
+		num1=entrada.nextInt();
+		
+		System.out.println("Ingrese numero 1");
+		num1=entrada.nextInt();
+		
+		System.out.println("Ingrese numero 2");
+		num2=entrada.nextInt();
+		
+		System.out.println("Ingrese numero 3");
+		num3=entrada.nextInt();
+		
+		System.out.println("Ingrese numero 4");
+		num4=entrada.nextInt();
+		
+		System.out.println("Ingrese numero 5");
+		num5=entrada.nextInt();
+		
+		promedio=(num1+num2+num3+num4+num5)/5;
+		
+		
+	
+		
+		
+		
+		
+
+	}
+
+}
