065210df6aa05d0a0c7fdd22fb056ae908a8ecbc
Sergio Hurtarte <hurtartesergio@gmail.com>
2018-03-01 21:22:17 -0600

    fibonacci

---
+++ OperacionesBasicas/src/ciclos/Solution15.java
@@ -0,0 +1,53 @@
+package ciclos;
+
+import java.util.Scanner;
+
+public class Solution15 {
+
+	public static void main(String[] args) {
+
+		int numeroUno = 0;
+		int numeroDos = 1;
+		int acumulador;
+		int hastaDonde;
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Ingrese un numero");
+		hastaDonde = entrada.nextInt();
+
+		System.out.println(numeroUno);
+		System.out.println(numeroDos);
+		// System.out.println(numeroDos);
+
+		for (int i = 0; i <= hastaDonde; i++) {
+			
+			acumulador = numeroUno;
+			numeroUno = numeroDos;
+			numeroDos = acumulador + numeroUno;
+			
+			if(numeroDos>hastaDonde) {
+				
+				System.out.println(" ");
+				
+				
+			}else {
+				System.out.println(numeroDos);
+				
+			}
+			
+			
+
+			if (numeroDos>=hastaDonde) {
+				
+				i = numeroDos;
+
+			}
+			
+			
+			
+
+		}
+		
+
+	}
+
+}
